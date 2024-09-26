64f9893c54b17b56ad59167ace41c7838ed1e3c2
123 <123456@123.com>
2024-09-26 21:18:36 +0800

    test

--- include/db_info.hpp
+++ include/db_info.hpp
@@ -1,5 +1,4 @@
 #pragma once
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
-extern json& db_config(json &);
-
+extern json &db_config(json &, const std::string &);
--- src/db_config/db_info.cpp
+++ src/db_config/db_info.cpp
@@ -3,18 +3,17 @@
 #include "db_info.hpp"
 #include <fstream>
 using json = nlohmann::json;
-const std::string config_file_path = "/root/config/db_config.json";
 
-json& db_config(json &config)
+json &db_config(json &config, const std::string &config_file_path)
 {
-	std::ifstream config_file {config_file_path};
-	if(!config_file.is_open())
+	std::ifstream config_file{config_file_path};
+	if (!config_file.is_open())
 	{
 		std::cerr << "Could not open the config file." << std::endl;
 		std::cerr << "Program has ended with code 1." << std::endl;
 		return config;
 	}
-	
+
 	config_file >> config;
 
 	config_file.close();
