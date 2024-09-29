15bfa14d6f1b926403c95d1a416e608edb339453
123 <123456@123.com>
2024-09-29 09:41:23 +0800

    insert改善

--- src/db_insert/insert.cpp
+++ src/db_insert/insert.cpp
@@ -8,6 +8,42 @@ using std::cout;
 using std::endl;
 const std::string path = "/home/arch_/Work/ProjectNotKnow/config/db_config.json";
 
+int addPassage(std::shared_ptr<sql::PreparedStatement> &stmnt,
+               sql::SQLString passage,
+               sql::SQLString author,
+               sql::SQLString source,
+               int32_t page_number)
+{
+    int last_id = -1;
+
+    try
+    {
+        stmnt->setString(1, passage);
+        stmnt->setString(2, author);
+        stmnt->setString(3, source);
+        stmnt->setInt(4, page_number);
+
+        stmnt->executeUpdate();
+
+        std::cout << "Successfully added the entry to the database." << std::endl;
+
+        std::shared_ptr<sql::PreparedStatement> id_stmt(
+            stmnt->getConnection()->prepareStatement("SELECT LAST_INSERT_ID()"));
+        std::unique_ptr<sql::ResultSet> res(id_stmt->executeQuery());
+        if (res->next())
+        {
+            last_id = res->getInt(1);
+        }
+    }
+    catch (sql::SQLException &e)
+    {
+        std::cerr << "Error adding entry to the database: "
+                  << e.what() << std::endl;
+    }
+
+    return last_id;
+}
+
 int getSummaryID(std::shared_ptr<sql::Statement> &stmnt, sql::SQLString summary)
 {
     int ret = -1;
@@ -87,30 +123,62 @@ int main(int argc, char *argv[])
         cerr << "用法: " << argv[0] << " <文段> <作者> <出处> <页数> <摘要>..." << std::endl;
         return -1;
     }
-    const std::string str = std::string{"insert_passage "} + '\"' + argv[1] + '\"' + ' ' + '\"' + argv[2] + '\"' + ' ' + '\"' + argv[3] + '\"' + ' ' + argv[4];
-    char *command = const_cast<char *>(str.c_str());
-    int res = system(command);
-    res = WEXITSTATUS(res);
+
+    for (int i = 1; i < 4; i++)
+    {
+        if (strcmp(argv[1], "") == 0)
+        {
+            std::cerr << "用法: " << argv[0] << " <文段> <作者> <出处> <页数> " << "不能为空！" << std::endl;
+            return -1;
+        }
+    }
+
+    sql::SQLString passage(argv[1]);
+    sql::SQLString author(argv[2]);
+    sql::SQLString source(argv[3]);
+    int page_number = std::stoi(argv[4]);
+
+    // const std::string str = std::string{"insert_passage "} + '\"' + argv[1] + '\"' + ' ' + '\"' + argv[2] + '\"' + ' ' + '\"' + argv[3] + '\"' + ' ' + argv[4];
+    // char *command = const_cast<char *>(str.c_str());
+    // int res = system(command);
+    // res = WEXITSTATUS(res);
 
     json config;
     db_config(config, path);
 
     const std::string url = std::string{"jdbc:mariadb://127.0.0.1:3306/"} + std::string{config["database"]};
 
-    for (int i = 5; i < argc; i++)
+    sql::Driver *driver = sql::mariadb::get_driver_instance();
+    sql::SQLString URL(url);
+
+    sql::Properties properties({{"user", static_cast<sql::SQLString>(config["username"])},
+                                {"password", static_cast<sql::SQLString>(config["password"])},
+                                {"useTls", "true"}});
+
+    std::unique_ptr<sql::Connection> conn = nullptr;
+
+    try
     {
-        sql::SQLString summary(argv[i]);
+        conn = std::unique_ptr<sql::Connection>{(driver->connect(URL, properties))};
+        conn->setAutoCommit(false);
+    }
+    catch (sql::SQLException &e)
+    {
+        std::cerr << "Error Connecting to the database: " << e.what() << std::endl;
+        return -1;
+    }
 
-        try
+    try
+    {
+        std::shared_ptr<sql::PreparedStatement> stmnt(
+            conn->prepareStatement(
+                "INSERT INTO entry.passages(passage, author, source, page_number) VALUES (?, ?, ?, ?)"));
+
+        int res = addPassage(stmnt, passage, author, source, page_number);
+
+        for (int i = 5; i < argc; i++)
         {
-            sql::Driver *driver = sql::mariadb::get_driver_instance();
-            sql::SQLString URL(url);
-
-            sql::Properties properties({{"user", static_cast<sql::SQLString>(config["username"])},
-                                        {"password", static_cast<sql::SQLString>(config["password"])},
-                                        {"useTls", "true"}});
-
-            std::unique_ptr<sql::Connection> conn(driver->connect(URL, properties));
+            sql::SQLString summary(argv[i]);
 
             std::shared_ptr<sql::PreparedStatement> stmnt(
                 conn->prepareStatement(
@@ -119,28 +187,34 @@ int main(int argc, char *argv[])
             std::shared_ptr<sql::Statement> stmnt_summary(
                 conn->createStatement());
 
-            int id_summary = getSummaryID(stmnt_summary, summary);
-
             std::shared_ptr<sql::PreparedStatement> stmnt_medium(
                 conn->prepareStatement(
                     "INSERT INTO entry.passage_summaries(passage_id,summary_id) VALUES (?, ?)"));
 
+            int id_summary = getSummaryID(stmnt_summary, summary);
+
             int result = -1;
+
             if ((result = getSummaryID(stmnt_summary, summary)) != -1)
                 ;
             else
                 result = addSummary(stmnt, summary);
 
             addMedium(stmnt_medium, res, result);
+        }
 
-            conn->close();
-        }
-        catch (sql::SQLException &e)
-        {
-            std::cerr << "Error Connecting to the database: " << e.what() << std::endl;
-            return -1;
-        }
+        conn->commit();
+        cout << "Transaction Committed Success." << endl;
     }
+    catch (sql::SQLException &e)
+    {
+        std::cerr << "Error Submit to the database: " << e.what() << std::endl;
+        conn->rollback();
+        std::cerr << "Fatal Error! Transaction rolled back" << std::endl;
+        return -1;
+    }
+
+    conn->close();
 
     return 0;
 }
