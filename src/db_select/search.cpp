ad58be97e19cdb2bce346a346b1680da72779aac
123 <123456@123.com>
2024-09-29 16:47:21 +0800

    search

--- src/db_select/search.cpp
+++ src/db_select/search.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <mariadb/conncpp.hpp>
-#include "db_info.hpp"
+#include <vector>
+// #include "db_info.hpp"
 using std::cerr;
 using std::cout;
 using std::endl;
 const std::string path = "/home/arch_/Work/ProjectNotKnow/config/db_config.json";
+std::vector<sql::SQLString> summaries;
 int main(int argc, char *argv[])
 {
+    for (int i = 1; i < argc; i++)
+    {
+    }
 }
